5b44b8a59e671b29476d40edeb774467ffab8b08
Thienbonn <thientu1032@gmail.com>
2023-05-19 14:42:56 +0700

    new

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const morgan = require('morgan');
-const handlebars = require("express-handlebars");
-const port = 2222;
+const handlebars = require("express-handlebars").engine;
+const port = 3000;
 
 app.use(morgan("combined"))
 
-// app.engine('handlebars', handlebars());
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.engine('hds', handlebars({
+//     extname: ".hds"
+// }));
+app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
+app.set("views" ,path.join(__dirname, "resources/views"))
+
+// console.log(__dirname)
 
 
 app.get('/', (req, res) => {
-    return res.send("Hello 3456!");
+    return res.render("home");
+})
+app.get('/new', (req, res) => {
+    return res.render("new");
 })
 
 app.listen(port, () => console.log(`test: listening on http://localhost:${port}`));
